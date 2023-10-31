62c4d99389a5a64277aab148799775f617168b54
Christian <valkanovc@gmail.com>
2023-10-31 11:37:08 -0400

    added decode function

--- main.py
+++ main.py
@@ -5,6 +5,21 @@ def add_three_to_digits(number):
         new_number += str((int(digit) + 3) % 10)
     return int(new_number)
 
+def decode(password):
+    pw = []
+    decoded_pass = []
+    string_pass = ""
+    for value in password:
+        pw.append(int(value))
+    for digit in pw:
+        digit -= 3
+        if digit < 0:
+            digit += 10
+        decoded_pass.append(digit)
+    for value in decoded_pass:
+        string_pass += str(value)
+    return string_pass
+
 while True:
     num = int(input("Enter number to encode: "))
     output = add_three_to_digits(num)
